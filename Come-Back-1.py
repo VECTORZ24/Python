7b7b4098e1305108af58bab893a1df1b4645f3fc
VECTORZ24 <gayth.gooty1@gmail.com>
2025-08-18 21:53:41 +0100

    Plus One (Easy script)

--- Come-Back-1.py
+++ Come-Back-1.py
@@ -1,15 +1,14 @@
-#Roman To Integer Language System
 class Solution(object):
-    D = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    S = 0
-
-    def romanToInt(self, s):
-        for i in range(len(s) - 1):
-            if (self.D[s[i]] >= self.D[s[i + 1]]):
-                self.S += self.D[s[i]]
-            else:
-                self.S -= self.D[s[i]]
-        self.S += self.D[s[len(s) - 1]]
-        return self.S
-X=Solution()
-print('I was born in',X.romanToInt('MMIII'))
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        Ch=''
+        for i in digits:
+            Ch=Ch+str(i)
+        N=int(Ch)+1
+        digits=[]
+        for i in str(N):
+            digits.append(int(i))
+        return digits
