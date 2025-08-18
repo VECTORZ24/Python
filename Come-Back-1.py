99a80a80111884707bcdd92f0bf422658838ad95
VECTORZ24 <gayth.gooty1@gmail.com>
2025-08-18 21:38:07 +0100

    Roman To Integer Language System

--- Come-Back-1.py
+++ Come-Back-1.py
@@ -1,23 +1,12 @@
-def binary_search_recursive(my_list, item):
-  L=sorted(my_list)
-  found=False
-  first =0
-  last=len(L)-1
-  mid=(first+last)//2
-  if(len(L)==1):
-    if(L[0]!=item):
-      found=False
-    else:
-      found=True
-  else:
-    while first<=last and found==False:
-      if(L[mid]==item):
-        found=True
-      elif(L[mid]<item):
-        first=mid+1
-      else:
-        last=mid-1
-  return found
-# Testing the function
-print(binary_search_recursive([1, 3, 5, 7, 9], 5))
-print(binary_search_recursive([1, 3, 5, 7, 9], 6))
+class Solution(object):
+    D = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    S = 0
+
+    def romanToInt(self, s):
+        for i in range(len(s) - 1):
+            if (self.D[s[i]] >= self.D[s[i + 1]]):
+                self.S += self.D[s[i]]
+            else:
+                self.S -= self.D[s[i]]
+        self.S += self.D[s[len(s) - 1]]
+        return self.S
